6a7f839baec2659f171d9e5d7ee2cd9c51662a79
sapphi-red <green@sapphi.red>
2019-03-30 15:00:39 +0900

    Update: init

---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+const gulp = require("gulp");
+const plumber = require("gulp-plumber");
+const pug = require("gulp-pug");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync");
+
+const paths = {
+  pug: "./src/**/*.pug",
+  sass: "./src/**/*.scss",
+  js: "./src/**/*.js",
+  img: "./src/img/**",
+  output: "./bin",
+  release: "./release",
+};
+
+const options = {
+  sass: {
+    output: "expanded",
+  },
+};
+
+gulp.task("html", () => {
+  return (
+    gulp
+      .src(paths.pug)
+      .pipe(pug())
+      .pipe(gulp.dest(paths.output))
+  );
+});
+
+gulp.task("css", () => {
+  return (
+    gulp
+      .src(paths.sass)
+      .pipe(sass(options.sass))
+      .pipe(gulp.dest(paths.output))
+  );
+});
+
+gulp.task("js", () => {
+  return (
+    gulp
+      .src(paths.js)
+      .pipe(gulp.dest(paths.output))
+  )
+});
+
+gulp.task("img", () => {
+  return (
+    gulp
+      .src(paths.img)
+      .pipe(gulp.dest(`${paths.output}/img`))
+  )
+});
+
+gulp.task("default", gulp.parallel("html", "css", "js", "img"));
+
+gulp.task("watch", gulp.parallel( () => {
+    browserSync({
+      server: { baseDir: paths.output }
+    });
+    gulp.watch(`${paths.output}`, gulp.task("reload"));
+  }, () => {
+    gulp.watch(paths.pug, gulp.task("html"));
+    gulp.watch(paths.sass, gulp.task("css"));
+    gulp.watch(paths.js, gulp.task("js"));
+    gulp.watch(paths.img, gulp.task("img"));
+  }
+));
+
+gulp.task("reload", (done) => {
+  browserSync.reload();
+  done();
+});
