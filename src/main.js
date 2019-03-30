06413b29fa7fe6d0624cbe0977c1eb916d9c1c4f
sapphi-red <green@sapphi.red>
2019-03-30 20:24:59 +0900

    Fix: main.jsのコミット忘れ

--- src/main.js
+++ src/main.js
@@ -1,1 +1,28 @@
-console.log("test");
+const $container = document.querySelector("#container");
+const $main = document.querySelector("#main");
+const $mainImg = $main.querySelector("img");
+
+let menu = false;
+
+$mainImg.addEventListener("mousedown", () => {
+  $mainImg.classList.add("focus");
+
+  let cancel = true;
+  setTimeout( () =>{
+    cancel = false;
+  }, 500);
+
+  $mainImg.addEventListener("mouseup", () => {
+    $mainImg.classList.remove("focus");
+    if (cancel) return;
+
+    $container.classList.replace("start", "menu");
+    menu = true;
+  });
+});
+
+$mainImg.addEventListener("click", () => {
+  if (!menu) return;
+
+  $main.classList.toggle("open");
+});
