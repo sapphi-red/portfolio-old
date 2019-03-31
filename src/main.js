1bcd95d6462291cbcc832f75ae2e8935dcc2b4dc
sapphi-red <green@sapphi.red>
2019-03-31 17:44:54 +0900

    Update: タッチ端末でも動作するように

--- src/main.js
+++ src/main.js
@@ -4,38 +4,65 @@ const $main = document.querySelector("#main");
 let menu = false;
 
 if (location.pathname === "/") {
-  const onMousedown = () => {
-    $main.classList.add("focus");
+  // PC
+  {
+    const onMousedown = () => {
+      $main.classList.add("focus");
 
-    let cancel = true;
-    const timeoutId = setTimeout( () =>{
-      cancel = false;
-    }, 500);
+      let cancel = true;
+      const timeoutId = setTimeout( () =>{
+        cancel = false;
+      }, 500);
 
-    const onMouseLeave = () => {
-      clearTimeout(timeoutId);
-      $main.classList.remove("focus");
-      $main.removeEventListener("mouseup", onMouseUp);
+      const onMouseLeave = () => {
+        clearTimeout(timeoutId);
+        $main.classList.remove("focus");
+        $main.removeEventListener("mouseup", onMouseUp);
+      };
+      $main.addEventListener("mouseleave", onMouseLeave);
+
+      const onMouseUp = () => {
+        clearTimeout(timeoutId);
+        $main.classList.remove("focus");
+        $main.removeEventListener("mouseleave", onMouseLeave);
+        if (cancel) return;
+
+        $container.classList.replace("start", "menu");
+        $main.removeEventListener("mousedown", onMousedown);
+        menu = true;
+      };
+      $main.addEventListener("mouseup", onMouseUp, { once: true });
     };
-    $main.addEventListener("mouseleave", onMouseLeave);
+    $main.addEventListener("mousedown", onMousedown);
 
-    const onMouseUp = () => {
-      clearTimeout(timeoutId);
-      $main.classList.remove("focus");
-      $main.removeEventListener("mouseleave", onMouseLeave);
-      if (cancel) return;
+    $main.addEventListener("dragstart", (e) => {
+      e.preventDefault();
+    });
+  }
 
-      $container.classList.replace("start", "menu");
-      $main.removeEventListener("mousedown", onMousedown);
-      menu = true;
+  // smartphone
+  {
+    const onTouchStart = () => {
+      $main.classList.add("focus");
+
+      let cancel = true;
+      const timeoutId = setTimeout( () =>{
+        cancel = false;
+      }, 500);
+
+      const onTouchEnd = () => {
+        clearTimeout(timeoutId);
+        $main.classList.remove("focus");
+        if (cancel) return;
+
+        $container.classList.replace("start", "menu");
+        $main.removeEventListener("touchstart", onTouchStart);
+        menu = true;
+      };
+      $main.addEventListener("touchend", onTouchEnd, { once: true });
     };
-    $main.addEventListener("mouseup", onMouseUp, { once: true });
-  };
-  $main.addEventListener("mousedown", onMousedown);
-
-  $main.addEventListener("dragstart", (e) => {
-    e.preventDefault();
-  });
+    $main.addEventListener("touchstart", onTouchStart);
+  }
 } else {
   menu = true;
 }
