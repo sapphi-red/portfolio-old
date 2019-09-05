dc0fd275ca428b4bd3968820b445c8efffda9c85
sapphi-red <green@sapphi.red>
2019-09-05 23:13:55 +0900

    Update: ogp

--- static.config.js
+++ static.config.js
@@ -1,4 +1,7 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/display-name */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
+import React from 'react'
 import { reloadClientData } from 'react-static/node'
 import path from 'path'
 import chokidar from 'chokidar'
@@ -24,7 +27,13 @@ export default {
         children: articles.map(article => ({
           path: `/articles/${article.data.frontmatter.slug}`,
           template: 'src/containers/Article',
-          getData: () => ({ article })
+          getData: () => ({
+            article,
+            ogp: {
+              title: `${article.data.frontmatter.title} - 翡翠置き場`,
+              description: article.data.frontmatter.description
+            }
+          })
         }))
       }
     ]
@@ -41,5 +50,35 @@ export default {
     require.resolve('react-static-plugin-reach-router'),
     require.resolve('react-static-plugin-sitemap')
   ],
-  siteRoot: '://green.sapphi.red'
+  siteRoot: 'green.sapphi.red',
+  Document: ({ Html, Head, Body, children, state }) => {
+    let title = 'green.sapphi.red'
+    let desc = '翠のポートフォリオ。'
+    if (state.routeInfo && state.routeInfo.data && state.routeInfo.data.ogp) {
+      const { ogp } = state.routeInfo.data
+      title = ogp.title
+      desc = ogp.description
+    }
+    return (
+      <Html lang="ja">
+        <Head>
+          <meta charSet="UTF-8" />
+          <title>{title}</title>
+          <meta name="description" content={desc} />
+          <meta property="og:title" content={title} />
+          <meta property="og:description" content={desc} />
+          <meta
+            property="og:url"
+            content={`https://green.sapphi.red/${state.route.path}`}
+          />
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:site" content="@sapphi_red" />
+          <meta name="og:img" content="https://green.sapphi.red/img/icon.png" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" type="image/png" href="/img/icon.png" />
+        </Head>
+        <Body>{children}</Body>
+      </Html>
+    )
+  }
 }
